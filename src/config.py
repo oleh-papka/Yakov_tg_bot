d63ed3321787c85654cabbbebc80d398feef7a81
Oleh Papka <olegpapka2@gmail.com>
2023-03-21 18:42:52 +0200

    Update: patch version bump

--- src/config.py
+++ src/config.py
@@ -44,7 +44,7 @@ class Config:
 
     SPACING = '⠀⠀  '  # Main whitespace characters used in formatting
 
-    BOT_VERSION = 'v0.1.2-1'
+    BOT_VERSION = 'v0.1.2-2'
 
     BOT_COMMANDS = [
         ('weather', 'Погода'),
